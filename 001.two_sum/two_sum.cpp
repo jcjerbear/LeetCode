f6f651f365c4e4e5029dec181fd56cfacd9c17f4
jcjerbear <cyc42@sfu.ca>
2016-12-19 17:57:04 -0800

    001submission1 - runtime600+ms

--- 001.two_sum/two_sum.cpp
+++ 001.two_sum/two_sum.cpp
@@ -1,15 +1,46 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int* twoSum(int* nums, int numsSize, int target)
-{
-	
-}
+class Solution{
+public:
+	vector<int> twoSum(vector<int>& nums, int target)
+	{
+		vector<int> indices;
+		for(vector<int>::size_type i=0;i<nums.size();i++)
+		{
+			for(vector<int>::size_type j=i+1;j<nums.size();j++)
+			{
+				//if the difference of target-nums[i] equals to the current checking value
+				if(nums[j] == target-nums[i])
+				{
+					indices.push_back(i);
+					indices.push_back(j);
+					return indices;
+				}
+			}
+		}
+		indices.clear();
+		return indices;
+	}
+};
 
 int main(int argc, char* argv[])
 {
-	// int array[1,2,3,4,5,6,7,8,9,10];
-
-
-
+	Solution twosum;
+	int array[] = {3,2,4};
+	vector<int> nums(array, array+sizeof(array)/sizeof(int));
+	vector<int> result;
+	
+	// for(vector<int>::size_type i=0; i<nums.size(); i++)
+	// {
+	// 	cout<<i<<":"<<nums[i]<<" ";
+	// }
+	result = twosum.twoSum(nums, 6);
+	for(vector<int>::iterator item=result.begin(); item!=result.end(); ++item)
+	{
+		cout<<*item<<" ";
+	}
+	cout<<endl;
+	return 0;
 }
