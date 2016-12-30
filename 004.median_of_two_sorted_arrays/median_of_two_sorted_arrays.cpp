d1f2eec0fb9e3106641ab3c9565e8281144398ab
jcjerbear <cyc42@sfu.ca>
2016-12-30 01:59:07 -0800

    004barebones

---
+++ 004.median_of_two_sorted_arrays/median_of_two_sorted_arrays.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        
+    }
+};
+
+int main(int argc, char* argv[])
+{
+	Solution result;
+	
+	return 0;
+}
