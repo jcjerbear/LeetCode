40098deacbef2e492553f2794ae93b359a1e302b
jcjerbear <cyc42@sfu.ca>
2016-12-30 01:43:39 -0800

    003submission2 - runtime128ms

--- 003.longest_substring_without_repeating_characters/longest_substring_without_repeating_characters.cpp
+++ 003.longest_substring_without_repeating_characters/longest_substring_without_repeating_characters.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <string>
-#include <set>
+#include <vector>
 using namespace std;
 
 class Solution {
 public:
+	/*
+	//submission1 - runtime error
     int lengthOfLongestSubstring(string s) {
         // cout<<s<<endl;
         int count=0;
@@ -17,7 +19,7 @@ public:
         // 	cout<<*iter<<" "<<endl;
         // }
 
-        for(int i=0;i<s.length()-1;i++)
+        for(int i=0;i<s.length();i++)
         {
         	// cout<<s[i]<<endl;
         	str.insert(s[i]);
@@ -35,15 +37,16 @@ public:
         		// if(s[i]!=s[j]) temp++;
         		// cout<<s[j]<<endl;
         		if(str.insert(s[j]).second) temp++;
-        		else break;
+           		else break;
         	}
         	// cout<<"count:"<<count<<endl;
         	// cout<<"temp:"<<temp<<endl;
         	if(temp>count) count=temp;
         	// for(set<char>::iterator iter=str.begin(); iter!=str.end(); ++iter)
         	// {
-        	// 	cout<<*iter<<" "<<endl;
+        	// 	cout<<*iter<<" ";
         	// }
+        	// cout<<endl<<endl;
 
         	str.clear();
         	temp=1;
@@ -53,13 +56,78 @@ public:
         }
         return count;
     }
+    */
+
+    //submission2 - runtime128ms
+    int lengthOfLongestSubstring(string s) {
+        vector<char> str;
+        int count=0;
+        int i=0;
+        int j=0;
+        
+        while(i<s.length() && j<s.length())
+        {
+        	cout<<"Beginning of errLoop"<<endl<<"print vector:"<<endl;
+        	for(vector<char>::iterator item=str.begin(); item!=str.end(); ++item)
+			{
+				cout<<*item<<" ";
+			}
+			cout<<endl;
+
+        	if(isUnique(str, s[j]))
+        	{
+        		cout<<"insert success"<<endl;
+        		str.push_back(s[j++]);
+        		for(vector<char>::iterator item=str.begin(); item!=str.end(); ++item)
+				{
+					cout<<*item<<" ";
+				}
+				cout<<endl;
+        		if((j-i)>count) count=j-i;
+        	}
+        	else
+        	{
+        		cout<<"insert failed"<<endl;
+        		str.erase(str.begin());
+        		for(vector<char>::iterator item=str.begin(); item!=str.end(); ++item)
+				{
+					cout<<*item<<" ";
+				}
+				cout<<endl;
+
+        		i++;
+        	}
+        	cout<<"i:"<<i<<endl;
+        	cout<<"j:"<<j<<endl;
+        	cout<<"count:"<<count<<endl;
+        	cout<<endl;
+        }
+        return count;
+    }
+
+    bool isUnique(vector<char> str, char element)
+    {
+    	cout<<"element:"<<element<<endl;
+    	cout<<"print vector:"<<endl;
+		for(vector<char>::iterator item=str.begin(); item!=str.end(); ++item)
+		{
+			cout<<*item<<" ";
+		}
+		cout<<endl;
+		// if(str.empty()) return 1;
+    	for(vector<char>::iterator item=str.begin(); item!=str.end(); ++item)
+    	{
+    		if(*item==element) return 0;
+    	}
+    	return 1;
+    }
 };
 
 int main(int argc, char* argv[])
 {
 	Solution result;
 	int length;
-	string str = "abcabcbb";
+	string str = "pwwkew";
 	length = result.lengthOfLongestSubstring(str);
 	cout<<length<<endl;
 	return 0;
