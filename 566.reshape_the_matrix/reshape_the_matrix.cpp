28d3c3be69a4b0fd00f6612360b968e33b3971bd
jcjerbear <cyc42@sfu.ca>
2017-06-20 00:01:23 -0700

    566barebones

---
+++ 566.reshape_the_matrix/reshape_the_matrix.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& nums, int r, int c) {
+        
+    }
+};
+
+int main(int argc, char* argv[])
+{
+    Solution result;
+
+    return 0;
+}
