6c4c673cbda6f82fa4eab12e1462ffdac8a5e8d5
jcjerbear <cyc42@sfu.ca>
2017-07-25 23:32:09 -0700

    617barebones

---
+++ 617.merge_two_binary_trees/merge_two_binary_trees.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ */
+ struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ };
+
+class Solution {
+public:
+    TreeNode *mergeTrees(TreeNode *t1, TreeNode *t2) {
+
+    }
+};
+
+int main(int argc, char *argv[]) {
+
+    Solution result;
+
+
+    return 0;
+}
