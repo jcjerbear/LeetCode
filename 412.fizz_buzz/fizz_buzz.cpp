b29ac7c59417921628e5bd820ec86e1194e56faf
jcjerbear <cyc42@sfu.ca>
2017-10-04 00:07:40 -0700

    412barebones

---
+++ 412.fizz_buzz/fizz_buzz.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+
+    }
+};
+
+int main() {
+    Solution result;
+    result.fizzBuzz(123);
+    return 0;
+}
