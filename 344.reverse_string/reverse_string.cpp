caf6de33fde3bb37bf7c25ed02dec5b35e0c9bf2
jcjerbear <cyc42@sfu.ca>
2017-10-03 23:34:26 -0700

    344submisson1 - runtime12ms

--- 344.reverse_string/reverse_string.cpp
+++ 344.reverse_string/reverse_string.cpp
@@ -5,12 +5,17 @@ using namespace std;
 class Solution {
 public:
     string reverseString(string s) {
-
+        string answer = "";
+        while (s != "") {
+            answer.push_back(s.back());
+            s.pop_back();
+        }
+        return answer;
     }
 };
 
 int main() {
     Solution result;
-    cout << result.reverseString("Jerry") << endl;
+    cout << result.reverseString("JerryChen") << endl;
     return 0;
 }
