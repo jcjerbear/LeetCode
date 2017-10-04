a1993925929e2b0d8c9fb0f7a386db1c6fd4149e
jcjerbear <cyc42@sfu.ca>
2017-10-04 01:17:29 -0700

    412submission1 - runtime9ms

--- 412.fizz_buzz/fizz_buzz.cpp
+++ 412.fizz_buzz/fizz_buzz.cpp
@@ -6,12 +6,23 @@ using namespace std;
 class Solution {
 public:
     vector<string> fizzBuzz(int n) {
-
+        vector<string> answer;
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) answer.push_back("FizzBuzz");
+            else if (i % 3 == 0) answer.push_back("Fizz");
+            else if (i % 5 == 0) answer.push_back("Buzz");
+            else answer.push_back(to_string(i));
+        }
+        return answer;
     }
 };
 
 int main() {
     Solution result;
-    result.fizzBuzz(123);
+    vector<string> answer;
+    answer = result.fizzBuzz(15);
+    for (vector<string>::iterator it = answer.begin(); it != answer.end(); ++it) {
+        cout << *it << ",";
+    }
     return 0;
 }
