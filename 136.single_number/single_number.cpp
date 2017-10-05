159b893d7e6e0e8910e4b99a305c7e5f26b13a5d
jcjerbear <cyc42@sfu.ca>
2017-10-04 22:15:20 -0700

    136submission1 - runtime23ms

--- 136.single_number/single_number.cpp
+++ 136.single_number/single_number.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
-    int singleNumber(vector<int>& nums) {
-
+    int singleNumber(vector<int> &nums) {
+        int output = 0;
+        for (vector<int>::iterator it = nums.begin(); it != nums.end(); ++it) {
+            output ^= *it;
+        }
+        return output;
     }
 };
 
 int main() {
     Solution result;
-    result.singleNumber();
+    int inputArray[] = {1, 2, 1, 2, 3};
+    vector<int> inputVector(inputArray, inputArray + sizeof(inputArray) / sizeof(int));
+    cout << result.singleNumber(inputVector) << endl;
 
     return 0;
 }
