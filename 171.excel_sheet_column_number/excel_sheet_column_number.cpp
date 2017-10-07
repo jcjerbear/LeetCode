7abc3246a3f39a420591807690ba2776257fcaa8
jcjerbear <cyc42@sfu.ca>
2017-10-07 16:38:08 -0700

    171.submission1 - runtime6ms

--- 171.excel_sheet_column_number/excel_sheet_column_number.cpp
+++ 171.excel_sheet_column_number/excel_sheet_column_number.cpp
@@ -5,12 +5,15 @@ using namespace std;
 class Solution {
 public:
     int titleToNumber(string s) {
-
+        int result = 0;
+        // Decimal value of A-Z on ASCII Table is 65-90
+        for (int i = 0; i < s.size(); result = result * 26 + (s.at(i) - 'A' + 1), i++);
+        return result;
     }
 };
 
 int main() {
     Solution result;
-    result.titleToNumber("AA");
+    cout << result.titleToNumber("AB") << endl;
     return 0;
 }
