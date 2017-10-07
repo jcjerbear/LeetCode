9ba4c5429ef9be5a6a9381768c81fc33e3513541
jcjerbear <cyc42@sfu.ca>
2017-10-07 03:40:21 -0700

    371.submission1 - runtime0ms

--- 371.sum_of_two_integers/sum_of_two_integers.cpp
+++ 371.sum_of_two_integers/sum_of_two_integers.cpp
@@ -5,12 +5,12 @@ using namespace std;
 class Solution {
 public:
     int getSum(int a, int b) {
-
+        return b == 0 ? a : getSum(a ^ b, (a & b) << 1);
     }
 };
 
 int main() {
     Solution result;
-    cout << result.getSum(1, 2) << endl;
+    cout << result.getSum(123, 321) << endl;
     return 0;
 }
