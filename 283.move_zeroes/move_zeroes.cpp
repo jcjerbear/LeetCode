b095e11c1ed8f52e4f3c054cb6701ac98a579aa0
jcjerbear <cyc42@sfu.ca>
2017-10-07 13:34:46 -0700

    283.barebones

---
+++ 283.move_zeroes/move_zeroes.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+
+    }
+};
+
+int main() {
+    Solution result;
+    result.moveZeroes();
+    return 0;
+}
