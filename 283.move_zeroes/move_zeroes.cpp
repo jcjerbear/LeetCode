1230fc352cc62e9376df0a53ea012c799c257f57
jcjerbear <cyc42@sfu.ca>
2017-10-07 15:07:09 -0700

    283.submission1 - runtime19ms

--- 283.move_zeroes/move_zeroes.cpp
+++ 283.move_zeroes/move_zeroes.cpp
@@ -5,13 +5,28 @@ using namespace std;
 
 class Solution {
 public:
-    void moveZeroes(vector<int>& nums) {
-
+    void moveZeroes(vector<int> &nums) {
+        int positionCounter = 0;
+        if (nums.size() == 0) return;
+        else {
+            for (int i = 0; i < nums.size(); i++) {
+                if (nums[i] != 0) {
+                    nums[positionCounter++] = nums[i];
+                }
+            }
+            while (positionCounter < nums.size()) {
+                nums[positionCounter++] = 0;
+            }
+        }
     }
 };
 
 int main() {
     Solution result;
-    result.moveZeroes();
+    int input[] = {0, 1, 1, 0, 1, 0, 1, 1};
+    vector<int> inputVector(input, input + sizeof(input) / sizeof(int));
+    result.moveZeroes(inputVector);
+    for (int i = 0; i < inputVector.size(); i++) cout << inputVector[i];
+
     return 0;
 }
