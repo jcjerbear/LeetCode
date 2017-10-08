d690e7838676f427e9185ce9da46067494690985
jcjerbear <cyc42@sfu.ca>
2017-10-08 16:13:50 -0700

    237.barebones

---
+++ 237.delete_node_in_a_linked_list/delete_node_in_a_linked_list.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+
+    }
+};
+
+int main() {
+    Solution result;
+    ListNode* inputList = new ListNode(1);
+    result.deleteNode(inputList);
+    return 0;
+}
