dfbd42ef0aeab7634f914d025877c020f9e40304
jcjerbear <cyc42@sfu.ca>
2017-10-07 17:52:20 -0700

    122.submission1 - runtime6ms

--- 122.best_time_to_buy_and_sell_stock_II /best_time_to_buy_and_sell_stock_II.cpp
+++ 122.best_time_to_buy_and_sell_stock_II /best_time_to_buy_and_sell_stock_II.cpp
@@ -6,7 +6,12 @@ using namespace std;
 class Solution {
 public:
     int maxProfit(vector<int> &prices) {
-
+        int total = 0;
+        if (prices.size() == 0) return 0;
+        for (int i = 0; i < prices.size() - 1; i++) {
+            if (prices[i + 1] > prices[i]) total += prices[i + 1] - prices[i];
+        }
+        return total;
     }
 };
 
