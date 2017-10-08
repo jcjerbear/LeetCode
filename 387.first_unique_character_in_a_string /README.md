67bd85ac8b055950a2d8eb1b4d4cd65094841957
jcjerbear <cyc42@sfu.ca>
2017-10-07 23:54:49 -0700

    387.barebones

---
+++ 387.first_unique_character_in_a_string /README.md
@@ -0,0 +1,12 @@
+# 387.first_unique_character_in_a_string
+
+Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1
+
+**Examples:**
+```
+s = "leetcode"
+return 0.
+
+s = "loveleetcode",
+return 2.
+```
---
+++ 387.first_unique_character_in_a_string /first_unique_character_in_a_string .cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+
+    }
+};
+
+int main() {
+    Solution result;
+    result.firstUniqChar("loveleetcode");
+    return 0;
+}
