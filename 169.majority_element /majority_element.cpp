731f1c652be5999809db8262ff40298d87f9c49e
jcjerbear <cyc42@sfu.ca>
2017-10-07 22:11:17 -0700

    169.submission1 - runtime22ms

--- 169.majority_element /majority_element.cpp
+++ 169.majority_element /majority_element.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
     int majorityElement(vector<int> &nums) {
-
+        unordered_map<int, int> hashTable;
+        for (int i = 0; i < nums.size(); i++) {
+            if (++hashTable[nums[i]] > nums.size() / 2) return nums[i];
+        }
     }
 };
 
@@ -14,6 +18,6 @@ int main() {
     Solution result;
     int input[] = {13, 15, 24, 15, 13, 15, 15};
     vector<int> inputVector(input, input + sizeof(input) / sizeof(int));
-    result.majorityElement(inputVector);
+    cout << result.majorityElement(inputVector) << endl;
     return 0;
 }
