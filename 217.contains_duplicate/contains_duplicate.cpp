14be4a3ed0f9cd54abfa69dd927ef71946acafe6
jcjerbear <cyc42@sfu.ca>
2017-10-09 14:07:39 -0700

    217.barebones

---
+++ 217.contains_duplicate/contains_duplicate.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int> &nums) {
+
+    }
+};
+
+int main() {
+    Solution result;
+    int input[] = {1, 2, 3, 5, 4, 5};
+    vector<int> inputVector(input, input + sizeof(input) / sizeof(int));
+    result.containsDuplicate(inputVector);
+    return 0;
+}
