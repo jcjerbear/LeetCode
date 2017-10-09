9049da6bf34d644b294a2500464b7d240a733850
jcjerbear <cyc42@sfu.ca>
2017-10-09 13:36:04 -0700

    013.submission1 - runtime85ms

--- 013.roman_to_integer/roman_to_integer.cpp
+++ 013.roman_to_integer/roman_to_integer.cpp
@@ -1,16 +1,38 @@
 #include <iostream>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
     int romanToInt(string s) {
+        unordered_map<char, int> hashTable = {{'I', 1},
+                                              {'V', 5},
+                                              {'X', 10},
+                                              {'L', 50},
+                                              {'C', 100},
+                                              {'D', 500},
+                                              {'M', 1000}};
 
+        int sum = hashTable[s.back()];
+        for (int i = s.size() - 2; i >= 0; --i) {
+            if (hashTable[s[i]] < hashTable[s[i + 1]]) {
+                sum -= hashTable[s[i]];
+            } else {
+                sum += hashTable[s[i]];
+            }
+        }
+
+        return sum;
     }
 };
 
 int main() {
     Solution result;
-    result.romanToInt("X");
+    cout << result.romanToInt("X") << endl;
+    cout << result.romanToInt("XV") << endl;
+    cout << result.romanToInt("XXI") << endl;
+    cout << result.romanToInt("DCXXI") << endl;
+
     return 0;
 }
