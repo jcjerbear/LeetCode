589aef2edb8cce5e8c4e06394bbe58f0a8b86bd4
jcjerbear <cyc42@sfu.ca>
2017-10-09 00:35:51 -0700

    242.submission - runtime16ms

--- 242.valid_anagram/valid_anagram.cpp
+++ 242.valid_anagram/valid_anagram.cpp
@@ -6,12 +6,20 @@ using namespace std;
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-
+        if (s.size() != t.size()) return false;
+        unordered_map<char, int> hashTable;
+        for (int i = 0; i < s.size(); ++hashTable[s[i]], --hashTable[t[i]], i++);
+        for (auto it:hashTable) {
+            if (it.second != 0) {
+                return false;
+            }
+        }
+        return true;
     }
 };
 
 int main() {
     Solution result;
-    result.isAnagram("anagram", "nagaram");
+    cout << result.isAnagram("anagram", "nagaram") << endl;
     return 0;
 }
