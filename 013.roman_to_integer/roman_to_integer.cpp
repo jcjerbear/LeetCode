00427c1bd08dd35d2161c4b8749064e265c2b1bd
jcjerbear <cyc42@sfu.ca>
2017-10-09 00:42:37 -0700

    013.barebones

---
+++ 013.roman_to_integer/roman_to_integer.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int romanToInt(string s) {
+
+    }
+};
+
+int main() {
+    Solution result;
+    result.romanToInt("X");
+    return 0;
+}
