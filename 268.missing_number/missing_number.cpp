27c97bdaace0122bce30183ab84272f04128f314
jcjerbear <cyc42@sfu.ca>
2017-10-10 01:00:47 -0700

    268.barebones

---
+++ 268.missing_number/missing_number.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int missingNumber(vector<int> &nums) {
+
+    }
+};
+
+int main() {
+    Solution result;
+    int input[] = {1, 2, 3, 5};
+    vector<int> inputVector(input, input + sizeof(input) / sizeof(int));
+    result.missingNumber(inputVector);
+    return 0;
+}
