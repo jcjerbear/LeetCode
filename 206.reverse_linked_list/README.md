a13e2e34dcf93f719336d1742558f41e1663bd5b
jcjerbear <cyc42@sfu.ca>
2017-10-09 17:00:54 -0700

    206.barebones

---
+++ 206.reverse_linked_list/README.md
@@ -0,0 +1,7 @@
+# 206.reverse_linked_list
+
+Reverse a singly linked list.
+
+__Hint:__
+
+A linked list can be reversed either iteratively or recursively. Could you implement both?
---
+++ 206.reverse_linked_list/reverse_linked_list.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+
+    }
+};
+
+int main() {
+    Solution result;
+    result.reverseList();
+    return 0;
+}
