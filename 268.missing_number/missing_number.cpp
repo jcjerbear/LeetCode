6f7e4bc1ada1f8e79d24beca7479fc7225f8bc17
jcjerbear <cyc42@sfu.ca>
2017-10-10 21:35:39 -0700

    268.submission1 - runtime36ms

--- 268.missing_number/missing_number.cpp
+++ 268.missing_number/missing_number.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
 #include <vector>
+#include <map>
 
 using namespace std;
 
 class Solution {
 public:
     int missingNumber(vector<int> &nums) {
-
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != i) return i;
+        }
+        return nums.size();
     }
 };
 
 int main() {
     Solution result;
-    int input[] = {1, 2, 3, 5};
+    int input[] = {1, 0};
     vector<int> inputVector(input, input + sizeof(input) / sizeof(int));
-    result.missingNumber(inputVector);
+    cout << result.missingNumber(inputVector) << endl;
     return 0;
 }
