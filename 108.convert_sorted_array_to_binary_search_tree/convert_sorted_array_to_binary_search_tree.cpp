ee3d96925a0e8d2583e302b41b091f08ac77d1a5
jcjerbear <cyc42@sfu.ca>
2017-10-11 10:25:12 -0700

    108.submission1 - runtime13ms

--- 108.convert_sorted_array_to_binary_search_tree/convert_sorted_array_to_binary_search_tree.cpp
+++ 108.convert_sorted_array_to_binary_search_tree/convert_sorted_array_to_binary_search_tree.cpp
@@ -15,7 +15,24 @@ struct TreeNode {
 class Solution {
 public:
     TreeNode *sortedArrayToBST(vector<int> &nums) {
+        if (nums.size() == 0) return NULL;
+        if (nums.size() == 1) return new TreeNode(nums[0]);
 
+        int mid = nums.size() / 2;
+        TreeNode *head = new TreeNode(nums[mid]);
+        vector<int> leftNums(nums.begin(), nums.begin() + mid);
+        vector<int> rightNums(nums.begin() + mid + 1, nums.end());
+        head->left = sortedArrayToBST(leftNums);
+        head->right = sortedArrayToBST(rightNums);
+        return head;
+    }
+
+    void preorderPrint(TreeNode *tree) {
+        if (tree) {
+            cout << tree->val;
+            preorderPrint(tree->left);
+            preorderPrint(tree->right);
+        }
     }
 };
 
@@ -23,6 +40,6 @@ int main() {
     Solution result;
     int input[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     vector<int> inputVector(input, input + sizeof(input) / sizeof(int));
-    result.sortedArrayToBST(inputVector);
+    result.preorderPrint(result.sortedArrayToBST(inputVector));
     return 0;
 }
