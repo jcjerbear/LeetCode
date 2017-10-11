ecda71e436efc2da55d8ccf3d64f4e10bc8a66c0
jcjerbear <cyc42@sfu.ca>
2017-10-11 15:07:37 -0700

    053.submission1 - runtime9ms

--- 053.maximum_subarray/maximum_subarray.cpp
+++ 053.maximum_subarray/maximum_subarray.cpp
@@ -6,7 +6,15 @@ using namespace std;
 class Solution {
 public:
     int maxSubArray(vector<int> &nums) {
+        int dp[nums.size()];//dp[i] means the maximum subarray ending with A[i];
+        dp[0] = nums[0];
+        int maxSum = dp[0];
 
+        for (int i = 1; i < nums.size(); i++) {
+            dp[i] = nums[i] + (dp[i - 1] > 0 ? dp[i - 1] : 0);
+            maxSum = max(maxSum, dp[i]);
+        }
+        return maxSum;
     }
 };
 
@@ -14,6 +22,6 @@ int main() {
     Solution result;
     int input[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
     vector<int> inputVector(input, input + sizeof(input) / sizeof(int));
-    result.maxSubArray(inputVector);
+    cout << result.maxSubArray(inputVector) << endl;
     return 0;
 }
