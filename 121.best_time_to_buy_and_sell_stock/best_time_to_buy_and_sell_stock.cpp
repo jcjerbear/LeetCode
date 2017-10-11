61180286662d55a2ceb755cfcff41d22c922f0b4
jcjerbear <cyc42@sfu.ca>
2017-10-11 12:32:48 -0700

    121.submission1 - runtime6ms

--- 121.best_time_to_buy_and_sell_stock/best_time_to_buy_and_sell_stock.cpp
+++ 121.best_time_to_buy_and_sell_stock/best_time_to_buy_and_sell_stock.cpp
@@ -6,7 +6,17 @@ using namespace std;
 class Solution {
 public:
     int maxProfit(vector<int> &prices) {
-
+        int minPrice = INT_MAX;
+        int maxProfit = 0;
+        if (prices.size() == 0 || prices.size() == 1) return 0;
+        for (int i = 0; i < prices.size(); ++i) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else if (prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+        return maxProfit;
     }
 };
 
@@ -14,6 +24,6 @@ int main() {
     Solution result;
     int input[] = {7, 1, 5, 3, 6, 4};
     vector<int> inputVector(input, input + sizeof(input) / sizeof(int));
-    result.maxProfit(inputVector);
+    cout << result.maxProfit(inputVector) << endl;
     return 0;
 }
