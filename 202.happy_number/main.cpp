1a2107ca169a4b6986d89343d3c1848d0518f181
jcjerbear <cyc42@sfu.ca>
2017-10-12 21:52:20 -0700

    202.barebones

---
+++ 202.happy_number/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isHappy(int n) {
+
+    }
+};
+
+int main() {
+    Solution result;
+    result.isHappy(100);
+    return 0;
+}
