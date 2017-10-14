14dc138397dbba578711c0474b36d951c0d300b1
jcjerbear <cyc42@sfu.ca>
2017-10-13 23:34:22 -0700

    326.barebones

---
+++ 326.power_of_three/README.md
@@ -0,0 +1,7 @@
+# 326.power_of_three
+
+Given an integer, write a function to determine if it is a power of three.
+
+__Follow up:__
+
+Could you do it without using any loop / recursion?
---
+++ 326.power_of_three/power_of_three.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+
+    }
+};
+
+int main() {
+    Solution result;
+    result.isPowerOfThree(27);
+    return 0;
+}
