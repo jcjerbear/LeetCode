431f2e8d93f33b5be00bd7fee5e8e619bc3c6e4e
jcjerbear <cyc42@sfu.ca>
2017-10-14 22:28:44 -0700

    070.barebones

---
+++ 070.climbing_stairs/climbing_stairs.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int climbStairs(int n) {
+
+    }
+};
+
+int main() {
+    Solution result;
+    result.climbStairs(1);
+    return 0;
+}
