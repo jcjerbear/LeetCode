0349d40b761867320a0972780ff473ed5afce75b
jcjerbear <cyc42@sfu.ca>
2017-10-15 22:12:08 -0700

    191.barebones

---
+++ 191.number_of_1_bits/number_of_1_bits.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+
+    }
+};
+
+int main() {
+    Solution result;
+    result.hammingWeight(11);
+    return 0;
+}
