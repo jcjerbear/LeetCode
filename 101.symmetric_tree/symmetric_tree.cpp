5e66016ba7d629582b61294eb3a5b1339050a901
jcjerbear <cyc42@sfu.ca>
2017-10-18 22:36:13 -0700

    101.barebones

---
+++ 101.symmetric_tree/symmetric_tree.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+
+    }
+};
+
+int main() {
+    Solution result;
+    result.isSymmetric();
+    return 0;
+}
