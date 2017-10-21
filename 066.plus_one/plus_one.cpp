ddcc99d4c068160f1e29390f8276de56b2e199d1
jcjerbear <cyc42@sfu.ca>
2017-10-20 23:27:44 -0700

    066.barebones

---
+++ 066.plus_one/plus_one.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+
+    }
+};
+
+int main() {
+    Solution result;
+    result.plusOne();
+    return 0;
+}
