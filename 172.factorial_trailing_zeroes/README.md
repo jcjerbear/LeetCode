75683a51070541560045c3578bb92cd68ddd72d4
jcjerbear <cyc42@sfu.ca>
2017-10-21 23:35:35 -0700

    172.barebones

---
+++ 172.factorial_trailing_zeroes/README.md
@@ -0,0 +1,5 @@
+# 172.factorial_trailing_zeroes
+
+Given an integer n, return the number of trailing zeroes in n!.
+
+__Note:__ Your solution should be in logarithmic time complexity.
---
+++ 172.factorial_trailing_zeroes/factorial_trailing_zeroes .cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int trailingZeroes(int n) {
+
+    }
+};
+
+int main() {
+    Solution result;
+    cout << result.trailingZeroes() << endl;
+    return 0;
+}
