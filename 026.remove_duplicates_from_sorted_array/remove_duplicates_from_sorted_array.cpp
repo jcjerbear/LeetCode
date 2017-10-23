d7e59cc9b617f4be35a685f2c4ea9cde05d6507f
jcjerbear <cyc42@sfu.ca>
2017-10-22 22:03:50 -0700

    026.barebones

---
+++ 026.remove_duplicates_from_sorted_array/remove_duplicates_from_sorted_array.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int> &nums) {
+
+    }
+};
+
+int main() {
+    Solution result;
+    cout << result.removeDuplicates() << endl;
+    return 0;
+}
