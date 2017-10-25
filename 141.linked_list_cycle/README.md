e7d0949aa7e9435894c9c5d31b3a0bef6d0ba987
jcjerbear <cyc42@sfu.ca>
2017-10-24 23:37:09 -0700

    141.barebones

---
+++ 141.linked_list_cycle/README.md
@@ -0,0 +1,8 @@
+# 141.linked_list_cycle
+
+Given a linked list, determine if it has a cycle in it.
+
+Follow up:
+
+Can you solve it without using extra space?
+
---
+++ 141.linked_list_cycle/linked_list_cycle.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+
+    }
+};
+
+int main() {
+    Solution result;
+    cout << result.hasCycle() << endl;
+    return 0;
+}
