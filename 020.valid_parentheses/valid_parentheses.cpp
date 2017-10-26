3ba6607b371dd193f75b7301b5f24921ba012df2
jcjerbear <cyc42@sfu.ca>
2017-10-25 23:36:01 -0700

    020.barebones

---
+++ 020.valid_parentheses/valid_parentheses.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+
+    }
+};
+
+int main() {
+    Solution result;
+    cout << result.isValid() << endl;
+    return 0;
+}
