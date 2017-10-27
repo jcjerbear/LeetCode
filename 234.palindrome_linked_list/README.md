7e047c9df7b558baa29b5732ed0833b588b414c7
jcjerbear <cyc42@sfu.ca>
2017-10-26 20:17:15 -0700

    234.barebones

---
+++ 234.palindrome_linked_list/README.md
@@ -0,0 +1,10 @@
+# 234.palindrome_linked_list
+
+Given a singly linked list, determine if it is a palindrome.
+
+__Follow up:__
+
+Could you do it in O(n) time and O(1) space?
+
+
+
---
+++ 234.palindrome_linked_list/palindrome_linked_list.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    bool isPalindrome(ListNode *head) {
+
+    }
+};
+
+int main() {
+    Solution result;
+    cout << result.isPalindrome() << endl;
+    return 0;
+}
