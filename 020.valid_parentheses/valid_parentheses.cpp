6e86db922cc4eba6f869aa12b94bc504d7ec520b
jcjerbear <cyc42@sfu.ca>
2017-11-13 12:09:36 -0800

    020.submission1 - runtime3ms

--- 020.valid_parentheses/valid_parentheses.cpp
+++ 020.valid_parentheses/valid_parentheses.cpp
@@ -1,16 +1,35 @@
 #include <iostream>
+#include <stack>
 
 using namespace std;
 
 class Solution {
 public:
     bool isValid(string s) {
+        stack<char> Stack;
+        int size = s.size();
 
+        for (int i = 0; i < size; i++) {
+            if (s[i] == '(' || s[i] == '[' || s[i] == '{') {
+                Stack.push(s[i]);
+            } else if (s[i] == ')') {
+                if (Stack.empty() || Stack.top() != '(') return false;
+                Stack.pop();
+            } else if (s[i] == ']') {
+                if (Stack.empty() || Stack.top() != '[') return false;
+                Stack.pop();
+            } else if (s[i] == '}') {
+                if (Stack.empty() || Stack.top() != '{') return false;
+                Stack.pop();
+            }
+        }
+
+        return Stack.empty();
     }
 };
 
 int main() {
     Solution result;
-    cout << result.isValid() << endl;
+    cout << result.isValid("()}") << endl;
     return 0;
 }
