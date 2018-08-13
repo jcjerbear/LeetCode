b3a9e529d9263cbf5b51e343fdfbf159e8249faa
jcjerbear <cyc42@sfu.ca>
2018-08-13 02:45:23 -0700

    448.submission2 - runtime84ms

--- 448.find_all_numbers_disappeared_in_an_array/main.cpp
+++ 448.find_all_numbers_disappeared_in_an_array/main.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 class Solution {
 public:
+    // submission 1 - runtime 1564 ms
+    /*
     vector<int> findDisappearedNumbers(vector<int> &nums) {
         vector<int> missingVector;
         vector<int> existedVector;
@@ -24,13 +26,31 @@ public:
         }
         return missingVector;
     }
+    */
+
+    vector<int> findDisappearedNumbers(vector<int> &nums) {
+        vector<int> result;
+        vector<bool> bits(nums.size() + 1, 0);
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (!bits[nums[i]]) {
+                bits[nums[i]] = 1;
+            }
+        }
+        for (int i = 1; i <= nums.size(); i++) {
+            if (!bits[i]) {
+                result.push_back(i);
+            }
+        }
+        return result;
+    }
 };
 
 int main() {
     Solution result;
     vector<int> resultVector;
-//    vector<int> inputVector = {4, 3, 2, 7, 8, 2, 3, 1};
-    vector<int> inputVector = {10,2,5,10,9,1,1,4,3,7};
+    vector<int> inputVector = {4, 3, 2, 7, 8, 2, 3, 1};
+//    vector<int> inputVector = {10, 2, 5, 10, 9, 1, 1, 4, 3, 7};
 
     resultVector = result.findDisappearedNumbers(inputVector);
     for (auto i:resultVector) {
